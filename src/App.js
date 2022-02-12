78df6990218df05881c4654fde5a25c86272cdda
marielesf <mariele.fonttana@gmail.com>
2022-02-12 00:21:11 +0000

    changes in app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    nome: "Sandra",
+    idade: 20,
+    comida: "Lasanha",
+    musica: ["help", "Malvadao 3", "Uma linda mulher"],
+  };
+  render() {
+    return (
+      <div>
+        <h1>{this.state.nome} </h1>
+        <h2>{this.state.idade} </h2>
+        <h3>{this.state.comida} </h3>
+        <ol>
+          <li>{this.state.musica[0]} </li>
+          <li>{this.state.musica[1]}</li>
+          <li>{this.state.musica[2]}</li>
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
